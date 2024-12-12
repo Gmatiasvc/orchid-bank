8aebb3a5017aee0519d62cd6f8b5613cf4337b0d
Gerardo <gmatiasvc@hotmail.com>
2024-12-12 11:20:30 -0500

    implemented strinfFormatter static methods in operationHandling methods

--- src/functions/operationHandling.java
+++ src/functions/operationHandling.java
@@ -9,44 +9,132 @@ import objects.operaciones.Transferencia;
 
 public class operationHandling {
     
-    public static Deposito registerDeposito(String MONTO, String PID, String SUCCESS, String ACC, String DNI){
+    public static Deposito registerDeposito(int MONTO, String SUCCESS, String ACC, String DNI){
         String UNIX = Long.toString(Instant.now().getEpochSecond());
-        Deposito obj = new Deposito(PID, UNIX, MONTO, "0", SUCCESS, ACC, DNI);
+        Deposito obj = new Deposito(stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO) , "0", SUCCESS, ACC, DNI);
         DataSave.appendOperationAccount(obj.getACC(), false, obj);
         return obj;
     }
 
-    public static Retiro registerRetiro(String MONTO, String PID, String SUCCESS, String ACC, String DNI){
+    public static Retiro registerRetiro(int MONTO, String SUCCESS, String ACC, String DNI){
         String UNIX = Long.toString(Instant.now().getEpochSecond());
-        Retiro obj = new Retiro(PID, UNIX, MONTO, "1", SUCCESS, ACC, DNI);
+        Retiro obj = new Retiro(stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "1", SUCCESS, ACC, DNI);
         DataSave.appendOperationAccount(obj.getACC(), false, obj);
         return obj;
     }
 
-    public static Transferencia registerTransferenciaEntrante(String ACCR, String ACCS, String PID, String MONTO, String SUCCESS){
+    public static Transferencia registerTransferenciaEntrante(String ACCR, String ACCS, int MONTO, String SUCCESS){
         String UNIX = Long.toString(Instant.now().getEpochSecond());
-        Transferencia obj = new Transferencia(ACCR, ACCS, PID, UNIX, MONTO, "2", SUCCESS);
+        Transferencia obj = new Transferencia(ACCR, ACCS, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "2", SUCCESS);
         DataSave.appendOperationAccount(obj.getACCR(), false, obj);
         return obj;
     }
 
-    public static Transferencia registerTransferenciaSaliente(String ACCR, String ACCS, String PID, String MONTO, String SUCCESS){
+    public static Transferencia registerTransferenciaSaliente(String ACCR, String ACCS,  int MONTO, String SUCCESS){
         String UNIX = Long.toString(Instant.now().getEpochSecond());
-        Transferencia obj = new Transferencia(ACCR, ACCS, PID, UNIX, MONTO, "3", SUCCESS);
+        Transferencia obj = new Transferencia(ACCR, ACCS, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "3", SUCCESS);
         DataSave.appendOperationAccount(obj.getACCS(), false, obj);
         return obj;
     }
 
-    public static Cheque registerChequeEntrante(String ACCR, String ACCS, String DNI, String CHEQUE, String PID, String MONTO, String SUCCESS){
+    public static Cheque registerChequeEntrante(String ACCR, String ACCS, String DNI, String CHEQUE, int MONTO, String SUCCESS){
         String UNIX = Long.toString(Instant.now().getEpochSecond());
-        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, PID, UNIX, MONTO, "4", SUCCESS);
+        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "4", SUCCESS);
         DataSave.appendOperationAccount(obj.getACCR(), false, obj);
         return obj;
     }
 
-    public static Cheque registerChequeSaliente(String ACCR, String ACCS, String DNI, String CHEQUE, String PID, String MONTO, String SUCCESS){
+    public static Cheque registerChequeSaliente(String ACCR, String ACCS, String DNI, String CHEQUE, int MONTO, String SUCCESS){
         String UNIX = Long.toString(Instant.now().getEpochSecond());
-        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, PID, UNIX, MONTO, "5", SUCCESS);
+        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "5", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCS(), false, obj);
+        return obj;
+    }   
+
+
+
+    public static Deposito registerDeposito(float MONTO, String SUCCESS, String ACC, String DNI){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Deposito obj = new Deposito(stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO) , "0", SUCCESS, ACC, DNI);
+        DataSave.appendOperationAccount(obj.getACC(), false, obj);
+        return obj;
+    }
+
+    public static Retiro registerRetiro(float MONTO, String SUCCESS, String ACC, String DNI){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Retiro obj = new Retiro(stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "1", SUCCESS, ACC, DNI);
+        DataSave.appendOperationAccount(obj.getACC(), false, obj);
+        return obj;
+    }
+
+    public static Transferencia registerTransferenciaEntrante(String ACCR, String ACCS, float MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Transferencia obj = new Transferencia(ACCR, ACCS, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "2", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCR(), false, obj);
+        return obj;
+    }
+
+    public static Transferencia registerTransferenciaSaliente(String ACCR, String ACCS,  float MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Transferencia obj = new Transferencia(ACCR, ACCS, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "3", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCS(), false, obj);
+        return obj;
+    }
+
+    public static Cheque registerChequeEntrante(String ACCR, String ACCS, String DNI, String CHEQUE, float MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "4", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCR(), false, obj);
+        return obj;
+    }
+
+    public static Cheque registerChequeSaliente(String ACCR, String ACCS, String DNI, String CHEQUE, float MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "5", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCS(), false, obj);
+        return obj;
+    }
+
+
+    
+    public static Deposito registerDeposito(double MONTO, String SUCCESS, String ACC, String DNI){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Deposito obj = new Deposito(stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO) , "0", SUCCESS, ACC, DNI);
+        DataSave.appendOperationAccount(obj.getACC(), false, obj);
+        return obj;
+    }
+
+    public static Retiro registerRetiro(double MONTO, String SUCCESS, String ACC, String DNI){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Retiro obj = new Retiro(stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "1", SUCCESS, ACC, DNI);
+        DataSave.appendOperationAccount(obj.getACC(), false, obj);
+        return obj;
+    }
+
+    public static Transferencia registerTransferenciaEntrante(String ACCR, String ACCS, double MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Transferencia obj = new Transferencia(ACCR, ACCS, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "2", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCR(), false, obj);
+        return obj;
+    }
+
+    public static Transferencia registerTransferenciaSaliente(String ACCR, String ACCS,  double MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Transferencia obj = new Transferencia(ACCR, ACCS, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "3", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCS(), false, obj);
+        return obj;
+    }
+
+    public static Cheque registerChequeEntrante(String ACCR, String ACCS, String DNI, String CHEQUE, double MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "4", SUCCESS);
+        DataSave.appendOperationAccount(obj.getACCR(), false, obj);
+        return obj;
+    }
+
+    public static Cheque registerChequeSaliente(String ACCR, String ACCS, String DNI, String CHEQUE, double MONTO, String SUCCESS){
+        String UNIX = Long.toString(Instant.now().getEpochSecond());
+        Cheque obj = new Cheque(ACCR, ACCS, DNI, CHEQUE, stringFormatter.pidGenerator(), UNIX, stringFormatter.montoFormatter(MONTO), "5", SUCCESS);
         DataSave.appendOperationAccount(obj.getACCS(), false, obj);
         return obj;
     }
